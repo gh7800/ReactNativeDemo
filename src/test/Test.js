344a9f381c21a859cae6473e599991a240f8c02a
wangs <wangshuai_wy@126.com>
2018-12-17 09:19:43 +0800

    '封装网络请求'

---
+++ src/test/Test.js
@@ -0,0 +1,10 @@
+import React ,{Component}from 'react';
+import {TextView} from 'react-native';
+
+class Test extends Component{
+    render(){
+        return <TextView />;
+    }
+
+}
+export default Test;
